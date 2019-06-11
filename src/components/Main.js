3c074fe1e8ea34673e1d3a881ff9fd0187f4b089
rijad998 <37544758+rijad998@users.noreply.github.com>
2019-06-12 00:00:17 +0200

    Update main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React from "react";
 
-function Main(){
-    return(
-        <div style={{textAlign: 'center'}}>
-                       
-        </div>
-    );
+function Main() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={this.state.key}
+        onSelect={key => this.setState({ key })}
+      >
+        <Tab eventKey="naturalPerson" title="Natural Person">
+          <Sonnet />
+        </Tab>
+        <Tab eventKey="legalEntity" title="Legal Entity">
+          <Sonnet />
+        </Tab>
+        <Tab eventKey="phoneNumber" title="Phone Number">
+          <Sonnet />
+        </Tab>
+        <Tab eventKey="address" title="Address">
+          <Sonnet />
+        </Tab>
+      </Tabs>
+    </div>
+  );
 }
 
-export default Main
-
-
-
+export default Main;
